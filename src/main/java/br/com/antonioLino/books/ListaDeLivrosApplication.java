8d3ec435ae9ec393a150e90a1b2bc2da0775fe7d
AntonioALino <antonioaplino@gmail.com>
2024-02-07 09:13:01 -0300

    Started project

---
+++ src/main/java/br/com/antonioLino/books/ListaDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonioLino.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antonioLino/books/ListaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonioLino.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
